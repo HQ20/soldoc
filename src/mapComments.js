73a4fcdec0c155badd5afe315b96d76ab0c1d067
Bernardo Vieira <bernardo5@live.com.pt>
2019-03-25 17:14:56 +0000

    fix bug in comment extract and make it + generic

--- src/mapComments.js
+++ src/mapComments.js
@@ -2,12 +2,16 @@ export default (input) => {
     const returnComments = new Map();
     // get original comments
     const comments = input.match(/\/\*\*(\s[ ]+\* [\S ]+)*\s+\*\/\s+function [a-zA-Z]+\(/gm);
-    // extract the @notice
-    const noticeComment = comments[0].match(/\* @notice ([\w\s*. ]+)/);
-    // clean up the comment
-    const readableComment = noticeComment[1].replace(/\s+\*/gm, '');
-    // put it in a map
-    const functioName = comments[0].match(/function (.*)\(/)[1];
-    returnComments.set(functioName, readableComment);
+    comments.forEach(comment => {
+        // extract only the comment
+        const cleanComment = comment.match(/\/\*\*[\W\w]+\*\//);
+        // extract the @dev
+        const devComment = cleanComment[0].match(/\* @dev ([\w\s*,.\[\]=\(\)':/#\- ]+)/);
+        // clean up the comment
+        const readableComment = devComment[1].replace(/\s+\*\/?/gm, '');
+        // put it in a map
+        const functioName = comment.match(/function ([a-zA-Z]+)\(/)[1];
+        returnComments.set(functioName, readableComment);
+    });
     return returnComments;
 }
