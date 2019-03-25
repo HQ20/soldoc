77405b2413cf261331ac65e723d5e8f2eb81175c
Bernardo Vieira <bernardo5@live.com.pt>
2019-03-25 17:15:13 +0000

    add basic tests to comment extractor

---
+++ test/mapCommentsTest.js
@@ -0,0 +1,61 @@
+import expect from 'expect.js';
+import fs from 'fs';
+
+import MapComments from '../src/mapComments';
+
+describe('MapComments', () => {
+    const filePath = 'test/contracts/OZERC20.sol';
+
+    it('extract single line no param/return valid comments', () => {
+        // read file
+        const input = fs.readFileSync(filePath).toString();
+        // get filtered comments
+        const comments = MapComments(input);
+        // verify
+        expect(comments.get('totalSupply').trim()).to.be('Total number of tokens in existence');
+    });
+    it('extract single line w/ param/return valid comments', () => {
+        // read file
+        const input = fs.readFileSync(filePath).toString();
+        // get filtered comments
+        const comments = MapComments(input);
+        // verify
+        expect(comments.get('balanceOf').trim()).to.be('Gets the balance of the specified address.');
+    });
+    it('extract multiline line w/ param/return valid comments', () => {
+        // read file
+        const input = fs.readFileSync(filePath).toString();
+        // get filtered comments
+        const comments = MapComments(input);
+        // verify
+        expect(comments.get('transferFrom').trim()).to.be('Transfer tokens from one address to another. '
+            + 'Note that while this function emits an Approval event, this is not required as per the '
+            + 'specification, and other compliant implementations may not emit the event.');
+    });
+    it('extract multiline line w/ code in valid comments', () => {
+        // read file
+        const input = fs.readFileSync(filePath).toString();
+        // get filtered comments
+        const comments = MapComments(input);
+        // verify
+        expect(comments.get('decreaseAllowance').trim()).to.be('Decrease the amount of tokens that an '
+            + 'owner allowed to a spender. approve should be called when '
+            + '_allowed[msg.sender][spender] == 0. To decrement allowed value is better '
+            + 'to use this function to avoid 2 calls (and wait until the first transaction is '
+            + 'mined) From MonolithDAO Token.sol Emits an Approval event.');
+    });
+    it('extract multiline line w/ link in valid comments', () => {
+        // read file
+        const input = fs.readFileSync(filePath).toString();
+        // get filtered comments
+        const comments = MapComments(input);
+        // verify
+        expect(comments.get('approve').trim()).to.be('Approve the passed address to spend the '
+            + 'specified amount of tokens on behalf of msg.sender. Beware that changing an '
+            + 'allowance with this method brings the risk that someone may use both the old '
+            + 'and the new allowance by unfortunate transaction ordering. One possible '
+            + "solution to mitigate this race condition is to first reduce the spender's "
+            + 'allowance to 0 and set the desired value afterwards: https://github.com'
+            + '/ethereum/EIPs/issues/20#issuecomment-263524729');
+    });
+});
