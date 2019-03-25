c25802142648cf70a0a39c08f61486d3fad710f1
Bernardo Vieira <bernardo5@live.com.pt>
2019-03-25 18:02:51 +0000

    cleanup index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import parser from 'solidity-parser-antlr';
-import mapComments from './mapComments';
+import MapComments from './mapComments';
 import fs from 'fs';
 
 
@@ -7,26 +7,10 @@ function prepareFromFile(filePath) {
     // read file
     const input = fs.readFileSync(filePath).toString();
     // parse it using solidity-parser-antlr
-    const ast = parser.parse(input)
+    const ast = parser.parse(input);
     // filter for contract definition
     const astContract = ast.children.filter((child) => child.type === 'ContractDefinition');
     // get filtered comments
-    const comments = mapComments(input);
+    const comments = MapComments(input);
     return {ast: astContract, comments};
 }
-
-const result = prepareFromFile('./src/Sample.sol');
-
-console.log(result.comments.get('...'));
-
-// extract multiline comments and function
-// \/\*\*(\s[ ]+\* [\S ]+)*\s+\*\/\s+function [a-zA-Z]+\(.*\)
-
-//extract @notice
-// \* @notice ([\w\s*. ]+)
-
-// generic
-// remove new lines and stars
-// \s+\*
-
-// solidity-parser-antlr is important to get variable types and function returns
