8240cef0e927b021dbab66ea95a879786bf080c2
Bernardo Vieira <bernardo5@live.com.pt>
2019-03-29 11:28:51 +0000

    get @dev,@param and @return comments

--- src/mapComments.js
+++ src/mapComments.js
@@ -6,18 +6,45 @@
 exports.mapComments = (input) => {
     const returnComments = new Map();
     // get original comments
-    const comments = input.match(/\/\*\*(\s[ ]+\* [\S ]+)*\s+\*\/\s+function [a-zA-Z0-9_]+\(/gm);
-    comments.forEach((comment) => {
+    const rawComments = input.match(/\/\*\*(\s[ ]+\* [\S ]+)*\s+\*\/\s+function [a-zA-Z0-9_]+\(/gm);
+    rawComments.forEach((comment) => {
         // extract only the comment
         const cleanComment = comment.match(/\/\*\*[\W\w]+\*\//);
-        // extract the @dev
-        // eslint-disable-next-line no-useless-escape
-        const devComment = cleanComment[0].match(/\* @dev ([\w\s*,.\[\]=\(\)':/#\- ]+)/);
-        // clean up the comment
-        const readableComment = devComment[1].replace(/\s+\*\/?/gm, '');
+        // split by types
+        const splitComments = cleanComment[0].split(/\* (@dev|@param|@return)/);
+        // start clean arrays
+        const paramComments = new Map();
+        let devComment = '';
+        let returnComment = '';
+        for (let c = 1; c < splitComments.length; c += 2) {
+            // if it's a param, extract the name
+            if (splitComments[c] === '@param') {
+                // get param name
+                const paramName = splitComments[c + 1].match(/ ([\w\d_]*) /)[1];
+                // clean up the comment
+                const readableComment = splitComments[c + 1].split(paramName)[1].replace(/\s+\*\/?/gm, '');
+                // add to array
+                paramComments.set(paramName, readableComment);
+            } else if (splitComments[c] === '@return') {
+                // clean up the comment
+                const readableComment = splitComments[c + 1].replace(/\s+\*\/?/gm, '');
+                returnComment = readableComment;
+            } else if (splitComments[c] === '@dev') {
+                // clean up the comment
+                const readableComment = splitComments[c + 1].replace(/\s+\*\/?/gm, '');
+                devComment = readableComment;
+            }
+        }
         // put it in a map
         const functioName = comment.match(/function ([a-zA-Z0-9_]+)\(/)[1];
-        returnComments.set(functioName, readableComment);
+        returnComments.set(
+            functioName,
+            {
+                dev: devComment,
+                return: returnComment,
+                params: () => (val, render) => paramComments.get(render(val)),
+            },
+        );
     });
     return returnComments;
 };
