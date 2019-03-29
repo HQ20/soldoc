047e073e6fd39a2e0659d4abd08d5abf7c997c20
Bernardo Vieira <bernardo5@live.com.pt>
2019-03-29 22:05:17 +0000

    add comments to transformTemplate method

--- src/index.js
+++ src/index.js
@@ -47,8 +47,18 @@ function mergeInfoFile(solidityFile) {
     return [rawContractData.ast[0].name, contractDataWithComments];
 }
 
+/**
+ * Using the given parameters, calls the Mustache engine
+ * and renders the HTML page.
+ * @param {string} templateFile Path for template file
+ * @param {string} contractName Contract name
+ * @param {object} contractData Contract data, containing ast and comments to be rendered
+ * @param {string} contractPath Path to contract file
+ */
 function transformTemplate(templateFile, contractName, contractData, contractPath) {
+    // read template into a string
     const templateContent = String(fs.readFileSync(templateFile));
+    // put all data together
     const view = {
         filePath: contractPath,
         contract: {
@@ -56,6 +66,7 @@ function transformTemplate(templateFile, contractName, contractData, contractPat
         },
         contractData,
     };
+    // calls the render engine
     const output = Mustache.render(templateContent, view);
     return output;
 }
