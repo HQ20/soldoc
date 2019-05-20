d93e5e1e4a6fb888f04a0a84ed95255d47a67819
Bernardo Vieira <bernardo5@live.com.pt>
2019-05-20 20:17:01 +0100

    improve pdf generation

--- src/pdf.js
+++ src/pdf.js
@@ -29,22 +29,96 @@ md.renderer.rules.emoji = (token, idx) => `<i class="em em-${token[idx].markup}"
 exports.generatePDF = (contractsPreparedData, outputFolder) => {
     let content = '';
     contractsPreparedData.forEach((contract) => {
-        //
-        content += `# ${contract.contractName}`;
-        // add functions name
+        // extract contracts comments
+        if (contract.contractData.contract !== undefined) {
+            const {
+                title, dev, notice, author,
+            } = contract.contractData.contract;
+            content += `# ${contract.contractName}`;
+            if (title.length > 0 || author.length > 0) {
+                content += '\n\r\\- ';
+                if (title.length > 0) {
+                    content += `${title} `;
+                }
+                if (author.length > 0) {
+                    content += `*by ${author}*`;
+                }
+            }
+            if (notice.length > 0) {
+                content += `\n\r${notice}`;
+            }
+            if (dev.length > 0) {
+                content += `\n\r*${dev}*`;
+            }
+        }
+        // extract constructor comments
+        if (contract.contractData.constructor !== null) {
+            const {
+                dev, notice, author,
+            } = contract.contractData.constructor.comments;
+            if (notice.length > 0) {
+                content += `\n\r${notice}`;
+            }
+            if (dev.length > 0) {
+                content += `\n\r*${dev}*`;
+            }
+            if (author.length > 0) {
+                content += `\n\r\\- *by ${author}*`;
+            }
+        }
+        // extract event comments
+        contract.contractData.events.forEach((event) => {
+            content += `\n\r## ${event.ast.name}`;
+            if (event.comments === undefined) {
+                return;
+            }
+            const {
+                notice, dev, author,
+            } = event.comments;
+            if (notice.length > 0) {
+                content += `\n\r${notice}`;
+            }
+            if (dev.length > 0) {
+                content += `\n\r**@dev** ${dev}`;
+            }
+            if (author.length > 0) {
+                content += `\n\r*by ${author}*`;
+            }
+            if (event.ast.parameters !== null) {
+                event.ast.parameters.parameters.forEach((commentInput) => {
+                    content += `\n\r* @param \`${commentInput.typeName.name}\` `
+                        + `**${commentInput.name}** ${event.comments.param.get(commentInput.name)}`;
+                });
+            }
+        });
+        // extract functions comments
         contract.contractData.functions.forEach((func) => {
-            content += `\n\r### ${func.ast.name}`;
-            content += `\n\r**@dev** ${func.comments.dev}`;
+            content += `\n\r## ${func.ast.name}`;
+            if (func.comments === undefined) {
+                return;
+            }
+            const {
+                notice, dev, author,
+            } = func.comments;
+            if (notice.length > 0) {
+                content += `\n\r* ${notice}`;
+            }
+            if (dev.length > 0) {
+                content += `\n\r**@dev** ${dev}`;
+            }
+            if (author.length > 0) {
+                content += `\n\r*by ${author}*`;
+            }
             if (func.ast.parameters !== null) {
                 func.ast.parameters.parameters.forEach((commentInput) => {
-                    content += `\n\r**@param** ${commentInput.typeName.name} `
-                    + `${commentInput.name} ${func.comments.paramComments.get(commentInput.name)}`;
+                    content += `\n\r* @param \`${commentInput.typeName.name}\` `
+                        + `**${commentInput.name}** ${func.paramComments.get(commentInput.name)}`;
                 });
             }
             if (func.ast.returnParameters !== null) {
-                func.ast.returnParameters.parameters.forEach((commentInput) => {
-                    content += `\n\r**@return** ${commentInput.typeName.name} `
-                    + `${commentInput.name} ${func.comments.return}`;
+                func.ast.returnParameters.parameters.forEach((commentOutput) => {
+                    content += `\n\r* @return \`${commentOutput.typeName.name}\` `
+                        + `${func.comments.return}`;
                 });
             }
         });
