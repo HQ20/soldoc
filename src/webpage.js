c9b451ffac23d116ec1cbe90462a9a72be3a2884
Bernardo Vieira <bernardo5@live.com.pt>
2019-05-29 11:33:06 +0100

    render README if it exists

--- src/webpage.js
+++ src/webpage.js
@@ -2,6 +2,30 @@ const fs = require('fs');
 const path = require('path');
 const Mustache = require('mustache');
 const emoji = require('node-emoji');
+// to render README.md
+const hljs = require('highlight.js');
+const mdemoji = require('markdown-it-emoji');
+const md = require('markdown-it')({
+    highlight(str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return `<pre class="hljs"><code>${
+                    hljs.highlight(lang, str, true).value
+                }</code></pre>`;
+                // eslint-disable-next-line no-empty
+            } catch (__) { }
+        }
+        return `<pre class="hljs"><code>${md.utils.escapeHtml(str)}</code></pre>`;
+    },
+    html: true,
+    linkify: true,
+    typographer: true,
+});
+
+// use mdemoji plugin
+md.use(mdemoji);
+// set mdemoji rules
+md.renderer.rules.emoji = (token, idx) => `<i class="twa twa-${token[idx].markup}"></i>`;
 
 
 /**
@@ -49,12 +73,12 @@ exports.generateDocumentation = (contractsPreparedData, outputFolder) => {
         });
         contractsStructure.push(contractInfo);
     });
+    // verify if the docs/ folder exist and creates it if not
+    const destinationDocsFolderPath = path.join(process.cwd(), outputFolder);
+    if (!fs.existsSync(destinationDocsFolderPath)) {
+        fs.mkdirSync(destinationDocsFolderPath);
+    }
     contractsPreparedData.forEach((contract) => {
-        // verify if the docs/ folder exist and creates it if not
-        const destinationDocsFolderPath = path.join(process.cwd(), outputFolder);
-        if (!fs.existsSync(destinationDocsFolderPath)) {
-            fs.mkdirSync(destinationDocsFolderPath);
-        }
         // transform the template
         const HTMLContent = transformTemplate(
             path.join(contract.currentFolder, 'src/template/index.html'),
@@ -70,4 +94,33 @@ exports.generateDocumentation = (contractsPreparedData, outputFolder) => {
             emoji.emojify(HTMLContent, null, formatEmojify),
         );
     });
+    // If there's a README...
+    if (fs.existsSync(path.join(process.cwd(), 'README.md'))) {
+        // insert into index.html
+        const templateContent = String(fs.readFileSync(
+            path.join(contractsPreparedData[0].currentFolder, 'src/template/index.html'),
+        ));
+        const READMEText = String(fs.readFileSync(path.join(process.cwd(), 'README.md'))).trim();
+        // render it, from markdown to html
+        const READMEconverted = md.render(READMEText);
+        const README = READMEconverted
+            .replace(new RegExp('<h1>', 'g'), '<h1 class="title is-1">')
+            .replace(new RegExp('<h2>', 'g'), '<h2 class="title is-2">')
+            .replace(new RegExp('<h3>', 'g'), '<h3 class="title is-3">')
+            .replace(new RegExp('<h4>', 'g'), '<h4 class="title is-4">')
+            .replace(new RegExp('<ul>', 'g'), '<ul class="menu-list">');
+        // console.log(result);
+        // put all data together
+        const view = {
+            contractsStructure,
+            README,
+        };
+        // calls the render engine
+        const output = Mustache.render(templateContent, view);
+        // write it to a file
+        fs.writeFileSync(
+            path.join(process.cwd(), outputFolder, 'index.html'),
+            output,
+        );
+    }
 };
